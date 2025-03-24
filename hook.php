7bc113c0a291dc93a3338b2fe93a1599c670a010
Nathanaël Renaud <github.1wz.perso@renaudna.fr>
2025-03-24 20:18:16 +0100

    Fix bug when parsing base64 content Add new regex

--- hook.php
+++ hook.php
@@ -26,22 +26,26 @@ function plugin_descriptionCompleter_uninstall()
  */
 function replace_description_content(CommonDBTM $item)
 {
-   $item->input["content"] = htmlentities(
-      preg_replace(
-         "/t_(\d+)/i",
-         '<a href="/front/ticket.form.php?id=$1" target="_blank" rel="noopener">$0</a>',
-         html_entity_decode($item->input["content"])
-      ),
-      ENT_NOQUOTES,
-      'utf-8'
-   );
+   $regexPatterns = [
+      ["pattern" => "/(\s|[rpmi]>)(t_0*(\d+))/i", "substitute" => '$1<a href="/front/ticket.form.php?id=$3" target="_blank" rel="noopener">$2</a>'],
+      ["pattern" => "/(\s|[rpmi]>)(ticket\s*0*(\d+))/i", "substitute" => '$1<a href="/front/ticket.form.php?id=$3" target="_blank" rel="noopener">$2</a>'],
+      ["pattern" => "/(\s|[rpmi]>)(REQ0*(\d+))/i", "substitute" => '$1<a href="/front/ticket.form.php?id=$3" target="_blank" rel="noopener">$2</a>'],
+      ["pattern" => "/(\s|[rpmi]>)(REC0*(\d+))/i", "substitute" => '$1<a href="/front/ticket.form.php?id=$3" target="_blank" rel="noopener">$2</a>'],
+      ["pattern" => "/(\s|[rpmi]>)(PO0*(\d+))/i", "substitute" => '$1<a href="/front/ticket.form.php?id=$3" target="_blank" rel="noopener">$2</a>'],
+   ];
 
-   $item->input["content"] = htmlentities(
-      preg_replace(
-         "/ticket\s(\d+)/i",
-         '<a href="/front/ticket.form.php?id=$1" target="_blank" rel="noopener">$0</a>',
-         html_entity_decode($item->input["content"])
-      ),
+   $decodedContent = html_entity_decode($item->input["content"]);
+
+   foreach ($regexPatterns as ["pattern" => $pattern, "substitute" => $substitute]) {
+      $decodedContent = preg_replace(
+         $pattern,
+         $substitute,
+         $decodedContent
+      );
+   }
+
+   $item->input['content'] = htmlentities(
+      $decodedContent,
       ENT_NOQUOTES,
       'utf-8'
    );
